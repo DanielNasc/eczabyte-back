dd9d031856d6b2d1fe6d5b6e9cbccb6a92ebd553
GersonFeDutra <gerson.creative.des@gmail.com>
2024-04-06 16:01:13 -0300

    chore: create CRUD methods to table User

---
+++ src/users/users.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/service/prisma.service';
+
+@Injectable()
+export class UsersService {
+  private prisma = new PrismaService();
+
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        email: createUserDto.email,
+        password: createUserDto.password,
+        username: createUserDto.username,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findMany({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        email: updateUserDto.email,
+        username: updateUserDto.username,
+        password: updateUserDto.password,
+      },
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
