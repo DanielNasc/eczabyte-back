31fb90d6736b89c2ffa1730c177e0a2730d2c216
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-07 16:21:20 -0300

    fix: change signin expires to 48 hours

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '172800s' }, // 48 hours
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
