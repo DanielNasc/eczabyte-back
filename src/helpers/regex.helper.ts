ea07eb42f3fe0db1fded28103c92b55b4722e99c
Vinicius Ramon <viniciusramonn007@gmail.com>
2024-04-08 18:51:37 -0300

    atualizando autenticacoes

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -1,6 +1,9 @@
 const password =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
 
+const phone = /^\(\d{2}\)\s\d{4,5}-\d{4}$/; 
+
 export const RegexHelper = {
   password,
+  phone
 };
