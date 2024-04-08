986c106a8a49ba36aab62daa7c7dfcc9fa26a30d
Vinicius Ramon <viniciusramonn007@gmail.com>
2024-04-08 18:24:14 -0300

    update update-user-dto.ts

--- src/modules/users/dto/update-user-dto.ts
+++ src/modules/users/dto/update-user-dto.ts
@@ -1,9 +1,35 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsEmail, Matches, IsOptional, MinLength } from 'class-validator';
+import { MessagesHelper } from 'src/helpers/messages.helper';
+import { RegexHelper } from 'src/helpers/regex.helper';
 
 export class UpdateUserDto {
-  @IsNotEmpty()
-  firstName: string;
+  @IsOptional()
+  @MinLength(3, {
+    message: 'O nome de usuário deve ter no mínimo 3 caracteres',
+  })
+  username?: string;
 
-  @IsNotEmpty()
-  lastName: string;
+  @IsOptional()
+  @IsEmail({}, {
+    message: 'O email deve ser um endereço de email válido',
+  })
+  email?: string;
+
+  @IsOptional()
+  @Matches(RegexHelper.password, {
+    message: MessagesHelper.PASSWORD_VALID,
+  })
+  @MinLength(8, {
+    message: 'A senha deve ter no mínimo 8 caracteres',
+  })
+  password?: string;
+
+  @IsOptional()
+  country?: string;
+
+  @IsOptional()
+  gender?: string;
+
+  @IsOptional()
+  birthdate?: string;
 }
